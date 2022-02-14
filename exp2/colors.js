4900a8c3be19b0ee29e4af39c3fdfeda6a8b89e8
swagsarthak <75614066+swagsarthak@users.noreply.github.com>
2022-02-14 11:18:56 +0530

    Add files via upload

---
+++ exp2/colors.js
@@ -0,0 +1,52 @@
+
+function changeRed()
+{
+document.getElementById('back').style.backgroundColor="red";
+
+}
+function changeGreen()
+{
+document.getElementById('back').style.backgroundColor="green";
+
+}
+function changeYellow()
+{
+document.getElementById('back').style.backgroundColor="yellow";
+
+}
+function changeBlue()
+{
+document.getElementById('back').style.backgroundColor="blue";
+
+}
+function changeBlack()
+{
+document.getElementById('back').style.backgroundColor="black";
+
+}
+function changePink()
+{
+document.getElementById('back').style.backgroundColor="pink";
+
+}
+function changeWhite()
+{
+document.getElementById('back').style.backgroundColor="white";
+
+}
+function changeBrown()
+{
+document.getElementById('back').style.backgroundColor="brown";
+
+}
+
+function changeOrange()
+{
+document.getElementById('back').style.backgroundColor="orange";
+
+}
+function changeViolet()
+{
+document.getElementById('back').style.backgroundColor="violet";
+
+}
